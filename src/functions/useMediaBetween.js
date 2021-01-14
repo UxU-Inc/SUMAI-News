b220375f9334d6b4f51254d2be3477a99bae6f5d
smworld01 <smworld01@naver.com>
2021-01-14 15:09:43 +0900

    update useMediaBetween

--- src/functions/useMediaBetween.js
+++ src/functions/useMediaBetween.js
@@ -1,11 +1,23 @@
 
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-export default function useMediaBetween(a, b) {
+function number(a, b) {
+    const theme = useTheme()
+
+    return useMediaQuery(theme.breakpoints.between(a, b));
+}
+function string(a, b) {
     const theme = useTheme()
 
     const aSize = theme.breakpoints.values[a]
     const bSize = theme.breakpoints.values[b]
 
     return useMediaQuery(theme.breakpoints.between(aSize, bSize));
+}
+
+export default function useMediaBetween(a, b) {
+    if(typeof a===typeof b) {
+        if(typeof a==="string") return number(a,b)
+        else return string(a,b)
+    }
 }

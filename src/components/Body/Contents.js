a9385b60c23d4d41cd5f20f8a21d762b08517d3c
tkarlz <tjdudrb97@gmail.com>
2021-01-14 22:24:53 +0900

    News card location Modified, add like button

--- src/components/Body/Contents.js
+++ src/components/Body/Contents.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -9,6 +9,8 @@ import { red } from '@material-ui/core/colors';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import Box from '@material-ui/core/Box';
 import axios from 'axios';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 import * as NewsAgency from './NewsAgency'
 
@@ -21,9 +23,9 @@ const useStyles = makeStyles((theme) => ({
   CardHeader: {
     borderBottom: '1px solid #e0e0e0',
     color: '#0000008a',
-    padding: '8px 8px 8px 16px',
+    padding: '8px 8px 0px 16px',
   },
-  a: {
+  imgBox: {
     width: '80px',
     height: '40px',
   },
@@ -48,19 +50,28 @@ function news_agency_logo(news_agency) {
   return '/images/news_agency/' + news_agency + '.png';
 }
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function RecipeReviewCard(props) {
   const classes = useStyles();
-  const { news_agency, title, summary, idx, currentId } = props;
+  const { news, currentId } = props;
+  const { idx, title, url, news_agency, summary,  clicked } = news;
   const [liked, setIiked] = useState(false)
   const [loginError, setLoginError] = useState(false)
 
+  useEffect(() => {
+    setIiked(!!clicked)
+  }, [clicked, idx]);
+
   const like = () => {
     if(currentId !== "") {
-      axios.post('http://localhost:3306/api/news/like', { currentId, idx })
+      const id = currentId
+      const sign = liked? -1:1
+      axios.post('http://localhost:3306/api/news/like', { id, idx, sign })  //링크 바꿔야됨
       .then((response) => {
         setIiked(!liked)
-        console.log("a")
       }).catch((error) => {
   
       })
@@ -69,31 +80,42 @@ export default function RecipeReviewCard(props) {
     }
   }
 
+  const snackBarHandleClose = (event, reason) => {
+    setLoginError(false)
+  }
+  
   return (
     <Card className={classes.root} variant="outlined">
       <CardHeader
         className={classes.CardHeader}
         title={
           <Box display="flex" alignItems="center" >
-            <a href={NewsAgency.Info[news_agency]?.url} className={classes.a}>
+            <Box onClick={() => window.open(NewsAgency.Info[news_agency]?.url)} className={classes.imgBox}>
               <img src={news_agency_logo(news_agency)} alt={news_agency} className={classes.imgLogo} />
-            </a>
+            </Box>
             <div style={{ flexGrow: 1 }} />
-            <IconButton style={{ padding: "5px" }} >
-              <ThumbUpAltIcon onClick={() => like()} color={liked? "primary":"inherit"} style={{ fontSize: "30px" }} />
+            <IconButton style={{ padding: "5px" }} onClick={() => like()}>
+              <ThumbUpAltIcon color={liked? "primary":"inherit"} style={{ fontSize: "30px" }} />
             </IconButton>
+            <Snackbar open={loginError} autoHideDuration={3000} onClose={snackBarHandleClose}>
+              <Alert onClose={snackBarHandleClose} severity="error">
+                로그인을 해주세요.
+              </Alert>
+            </Snackbar>
           </Box>
         }
         subheader={
-          <Typography variant="h6" style={{ color: "#000" }}>
-            {title}
-          </Typography>
+          <Box onClick={() => window.open(url)} style={{textDecoration: 'none', cursor:'pointer', paddingBottom:"8px"}}>
+            <Typography variant="h6" style={{ color: "#000" }}>
+              {title}
+            </Typography>
+          </Box>
         }
       >
 
       </CardHeader>
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
+      <CardContent onClick={() => window.open(url)} style={{textDecoration: 'none', cursor:'pointer'}}>
+        <Typography variant="body2" color="textSecondary" component="p" style={{pointerEvents: "none", userSelect: "none"}}>
           {summary}
         </Typography>
       </CardContent>
